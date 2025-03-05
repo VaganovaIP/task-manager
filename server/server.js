69754af08f43d4c884290243f4801efc20b39408
Ваганова Ирина <iamvaganovanz@gmail.com>
2025-03-05 13:15:38 +0500

    подключение самоподписанного сертификата ssl

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const https = require('https');
 const express = require("express");
 require('dotenv').config();
 const db = require('./config/db.js');
@@ -7,22 +8,29 @@ const bodyParser = require("express");
 const passport = require("passport");
 require('./config/passport');
 const {v4: uuidv4} = require("uuid");
+const fs = require("fs");
 const port = process.env.PORT;
 
-
 const app = express();
+const options = {
+    key: fs.readFileSync('./security/localhost.key'),
+    cert: fs.readFileSync('./security/localhost.crt')
+};
+
 app.use(passport.initialize());
 app.use(express.static(__dirname));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+
 app.use('/', router);
 
 db.sequelize.sync({force:false})
     .then(()=>{
-        console.log(`Listen server port ${port}`)
-        app.listen(port);
+        https.createServer(options, app).listen(port, () => {
+            console.log(`Listen server port ${port}`)
+        });
     })
     .catch(err => console.log(err))
 
